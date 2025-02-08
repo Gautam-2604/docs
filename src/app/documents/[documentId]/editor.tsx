5782a2508acb7b015602b934fa94e5818258ed8f
Gautam-2604 <gaurisariag@gmail.com>
2025-02-08 16:14:35 +0530

    Toolbar created

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -3,15 +3,23 @@ import React from 'react'
 import { useEditor, EditorContent } from '@tiptap/react'
 import TaskItem from '@tiptap/extension-task-item'
 import TaskList from '@tiptap/extension-task-list'
+import Link from '@tiptap/extension-link'
+import {Color} from '@tiptap/extension-color'
+import Highlight from '@tiptap/extension-highlight'
 import Table from '@tiptap/extension-table'
+import TextStyle from '@tiptap/extension-text-style'
 import ImageResize from 'tiptap-extension-resize-image'
 import TableCell from '@tiptap/extension-table-cell'
 import Image from '@tiptap/extension-image'
 import TableHeader from '@tiptap/extension-table-header'
 import TableRow from '@tiptap/extension-table-row'
 import StarterKit from '@tiptap/starter-kit'
+import FontFamily from '@tiptap/extension-font-family'
+import Underline from '@tiptap/extension-underline'
+import { useEditorStore } from '@/store/use-editor-store'
 
 const Editor = () => {
+  const {setEditor} = useEditorStore()
   const editor = useEditor({
   editorProps:{
     attributes:{
@@ -19,7 +27,30 @@ const Editor = () => {
         class:"focus:outline-none print:border-0 bg-white border border-[#C7C7C7] flex flex-col min-h-[1054px] w-[816px] pt-10 pr-14 pb-10  cursor-text"
     }
   },
-  extensions: [StarterKit, TaskItem.configure({nested: true}), TaskList, Table.configure({resizable: true}), TableRow, TableHeader, TableCell, Image, ImageResize],
+  onCreate({editor}) {
+      setEditor(editor )
+  },
+  onDestroy() {
+      setEditor(null)
+  },
+  onSelectionUpdate({editor}) {
+      setEditor(editor)
+  },
+  onTransaction({editor}) {
+    setEditor(editor)
+  },
+  onFocus({editor}) {
+    setEditor(editor)
+  },
+  onBlur({editor}) {
+    setEditor(editor)
+  },
+  onContentError({editor}) {
+    setEditor(editor)
+  },
+  extensions: [StarterKit, TaskItem.configure({nested: true}), TaskList, Table.configure({resizable: true}), TableRow, TableHeader, TableCell, Image, ImageResize, Underline, FontFamily, TextStyle, Color, Highlight, Link.configure({openOnClick: false,
+                  autolink: true,
+                  defaultProtocol: 'https',})],
   content: `<table>
   <tbody>
     <tr>
--- src/app/documents/[documentId]/page.tsx
+++ src/app/documents/[documentId]/page.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Editor from './editor'
+import Toolbar from './toolbar'
 
 const DocumentIdPage = async ({params}:{params:Promise<{documentId: string}>}) => {
     const documentId = (await params).documentId
   return (
     
     <div className='min-h-screen bg-[#FAFBFD]'>
+      <Toolbar />
       <Editor />
+      
     </div>
   )
 }
