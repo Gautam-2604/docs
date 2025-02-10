600381f27f84d984d74cea23a1aef3a86b6350a6
Gautam-2604 <gaurisariag@gmail.com>
2025-02-10 11:30:17 +0530

    ruler added

--- src/app/documents/[documentId]/editor.tsx
+++ src/app/documents/[documentId]/editor.tsx
@@ -17,6 +17,7 @@ import StarterKit from '@tiptap/starter-kit'
 import FontFamily from '@tiptap/extension-font-family'
 import Underline from '@tiptap/extension-underline'
 import { useEditorStore } from '@/store/use-editor-store'
+import Ruler from './ruler'
 
 const Editor = () => {
   const {setEditor} = useEditorStore()
@@ -64,12 +65,11 @@ const Editor = () => {
       <td>Actress</td>
     </tr>
   </tbody>
-</table>
-<img src="https://placehold.co/800x400" />
-        <img src="https://placehold.co/800x400/6A00F5/white" />`,
+</table>`,
   })
   return (
     <div className='size-full overflow-x-auto bg-[#F9FBFD] px-4 print:p-0 print:bg-white print:overflow-visible '>
+      <Ruler />
       <div className='min-w-max flex justify-center w-[816px] py-4 print:py-0 mx-auto print:w-full print:min-w-0'>
         <EditorContent editor={editor} />
       </div>
---
+++ src/app/documents/[documentId]/ruler.tsx
@@ -0,0 +1,89 @@
+import React, { useRef, useState } from 'react'
+import {FaCaretDown} from 'react-icons/fa'
+const markers = Array.from({length: 83}, (_,i)=>i)
+
+const Ruler = () => {
+    const [leftMargin, setLeftMargin] = useState(56)
+    const [rightMargin, setRightMargin] = useState(56)
+    const [isDraggingLeft, setIsDraggingLeft] = useState(false)
+    const [isDraggingRight, setIsDraggingRight] = useState(false)
+    const rulerRef = useRef<HTMLDivElement>(null)
+
+    const handleLeftMouseDown = () =>{
+        setIsDraggingLeft(true)
+    }
+
+    const handleRightMouseDown = () =>{
+        setIsDraggingRight(true)
+    }
+
+    const handleMouseEvent = (e: React.MouseEvent)
+  return (
+    <div className='h-6 border-b border-gray-300 flex items-end relative select-none print:hidden'>
+      <div className='max-w-[816px] mx-auto w-full h-full relative' id='ruler-container'>
+        <Marker 
+        position={56}
+        isLeft={true}
+        isDragging={false}
+        onMouseDown={()=>{}}
+        onDoubleClick={()=>{}}
+        />
+        <Marker 
+        position={56}
+        isLeft={false}
+        isDragging={false}
+        onMouseDown={()=>{}}
+        onDoubleClick={()=>{}}
+        />
+        <div className='inset-x-0 absolute bottom-0 h-full'>
+            <div className='relative h-full w-[816px]'>
+                {markers.map((marker)=>{
+                    const position = (marker*816)/82;
+                    return(
+                        <div key={marker} className='absolute bottom-0' style={{left:`${position}px`}}>
+
+                            {marker%10===0 && (
+                                <>
+                                    <div className='absolute bottom-0 w-[1px] h-2 bg-neutral-500' />
+                                    <span className='absolute bottom-2 text-[10px] text-neutral-500 transform -translate-x-1/2'>
+                                        {marker/10+1}
+                                    </span>
+                                </>
+                            )}
+                            {marker%5===0 && marker%10!==0 && (
+                                <div className='absolute bottom-0 w-[1px] h-1.5 bg-neutral-500' />
+                            )}
+                            {marker%5!==0 && (
+                                <div className='absolute bottom-0 w-[1px] h-1 bg-neutral-500' />
+                            )}
+                        </div>
+                    )
+                })}
+            </div>
+
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
+
+export default Ruler
+
+interface MarkerProps{
+    position: number,
+    isLeft: boolean,
+    isDragging: boolean,
+    onMouseDown: ()=>void,
+    onDoubleClick:()=> void
+};
+
+const Marker = ({position, isLeft, isDragging, onMouseDown, onDoubleClick}:MarkerProps) =>{
+    return(
+    <div className='absolute top-0 w-4 h-full cursor-ew-resize z-[5] group -ml-2' style={{[isLeft?"left":"right"]:`${position}px`}} onMouseDown={onMouseDown} onDoubleClick={onDoubleClick}>
+        <FaCaretDown className='absolut left-1/2 top-0 h-full fill-blue-500 transform -translate-x-1/2 ' />
+    </div>
+    )
+
+}
