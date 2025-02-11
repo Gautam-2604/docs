8c8d8c318c489b54ad9307a1cb52081c7cb76943
Gautam-2604 <gaurisariag@gmail.com>
2025-02-12 01:25:45 +0530

    convex addition completed and done

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -6,13 +6,24 @@ import { useQuery } from 'convex/react'
 import { api } from '../../../convex/_generated/api'
 
 const Home = () => {
-  const documents = useQuery(api.document.get);
+  const documents =  useQuery(api.documents.get);
+  if(documents===undefined){
+    return(
+      <div>
+        Loading...
+      </div>
+    )
+  }
+
   return (
     <div className='flex flex-col min-h-screen '>
       <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4'>
         <Navbar />
       </div>
       <TemplateGallery />
+      {documents?.map((document)=>(
+        <span key={document._id}>{document.title}</span>
+      ))}
     </div>
   )
 }
