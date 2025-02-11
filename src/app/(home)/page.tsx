e798f8f770244e081d94058f96c995625918452a
Gautam-2604 <gaurisariag@gmail.com>
2025-02-12 01:09:21 +0530

    some changes

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,8 +1,12 @@
+'use client'
 import React from 'react'
 import Navbar from './navbar'
 import TemplateGallery from './template-gallery'
+import { useQuery } from 'convex/react'
+import { api } from '../../../convex/_generated/api'
 
 const Home = () => {
+  const documents = useQuery(api.document.get);
   return (
     <div className='flex flex-col min-h-screen '>
       <div className='fixed top-0 left-0 right-0 z-10 h-16 bg-white p-4'>
